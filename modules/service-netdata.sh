9e491520c8b5f08ce0c59b22d3dcbfd8bae76bf3
info@angelhost.eu <info@angelhost.eu>
2022-08-17 12:40:34 +0200

    Netdata TMPDIR for /tmp noexec

--- modules/service-netdata.sh
+++ modules/service-netdata.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Netdata
+
+## if /tmp has noexec
+export TMPDIR=/opt/tmp
+
 cd /opt/tmp
 curl -SsO https://my-netdata.io/kickstart-static64.sh
 sh ./kickstart-static64.sh --stable-channel --dont-wait --claim-token xxxxxxxxxxxxxxxx --claim-rooms xxxxxxxxxxxxxx --claim-url https://app.netdata.cloud
