4fee80b50c2cc979c40dbad0a028a7bed9c7698b
Takács Péter <info@angelhost.eu>
2022-08-27 12:36:26 +0200

    Netdata TMPDIR env to get autoupdates working

--- modules/service-netdata.sh
+++ modules/service-netdata.sh
@@ -5,7 +5,10 @@
 export TMPDIR=/opt/tmp
 
 cd /opt/tmp
+mkdir /opt/tmp/netdata_tmp
 curl -SsO https://my-netdata.io/kickstart-static64.sh
 sh ./kickstart-static64.sh --stable-channel --dont-wait --claim-token xxxxxxxxxxxxxxxx --claim-rooms xxxxxxxxxxxxxx --claim-url https://app.netdata.cloud
 sleep 2
+chown netdata:netdata /opt/tmp/netdata_tmp
+sed -i 's/NETDATA_TMPDIR=.*/NETDATA_TMPDIR="/opt/tmp/netdata_tmp"/g' /opt/netdata/etc/netdata/.environment
 rm -rf /opt/tmp/*
